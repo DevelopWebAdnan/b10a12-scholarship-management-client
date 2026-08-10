2dfd92efc17bdf53f7202582bcb4fbf942e67845
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-08-10 16:54:23 +0600

    Verify token and axios interceptor

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,14 +5,14 @@ const ErrorPage = () => {
     console.log(error);
 
     return (
-        <div>
+        <div className="text-center my-6">
             <h2 className="text-3xl">Ooppss!!</h2>
             <p>Sorry, an unexpected error has occurred.</p>
-            <p className="text-red-700 font-bold">{error.statusText || error.message}</p>
+            <p className="text-red-700 font-bold text-xl my-4">{error.statusText || error.message}</p>
             {
                 error.status === 404 && <div>
-                    <h3 className="text-2xl">Page Not Found</h3>
-                    Go back to <Link to="/"><button>Home</button></Link>
+                    <h3 className="text-lg">Page Not Found</h3>
+                    Go back to <Link to="/"><button className="btn bg-teal-500 text-white">Home</button></Link>
                 </div>
             }
         </div>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,7 +5,27 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
-    return axiosSecure;    
+    // request interceptor to be added to every valid call to the api
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = config.headers.authorization.split(' ')[1];
+        console.log('request intercepted by interceptors', token);
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
+
+    // handle 401 and 403 status
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, (error) => {
+        const status = error.response.status;
+        console.log('Status in the response interceptors', status);
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+    })
+    return axiosSecure;
 };
 
 export default useAxiosSecure;
