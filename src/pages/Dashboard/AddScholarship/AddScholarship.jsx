e6386cda403672a403ff1a781d963c50e5096e2b
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-08-11 10:01:07 +0600

    Created add scholarship form using react hook form and daisyUI

--- src/pages/Dashboard/AddScholarship/AddScholarship.jsx
+++ src/pages/Dashboard/AddScholarship/AddScholarship.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 
 
@@ -8,45 +9,144 @@ const AddScholarship = () => {
     }
     // const handleAddScholarship = 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("name")} />
-            <input {...register("university_name")} />
-            <input {...register("image/logo")} />
-            <input {...register("country")} />
-            <input {...register("city")} />
-            <input {...register("world_rank")} />
+        <div>
+            <Helmet>
+                <title>Scholarship Manager | Add Scholarship</title>
+            </Helmet>
+            {/* <Cover title="Add Scholarship"></Cover> */}
 
-            {/* <select {...register("subject_category")}>
-                <option value="female">female</option>
-                <option value="male">male</option>
-                <option value="other">other</option>
-            </select> */}
-            <select defaultValue="Pick a subject category" className="select">
-                <option disabled={true}>Pick a subject category</option>
-                <option>Agriculture</option>
-                <option>Engineering</option>
-                <option>Doctor</option>
-            </select>
-            <select defaultValue="Pick a scholarship category" className="select">
-                <option disabled={true}>Pick a scholarship category</option>
-                <option>Full-fund</option>
-                <option>Partial</option>
-                <option>Self-fund</option>
-            </select>
-            <select defaultValue="Pick a degree" className="select">
-                <option disabled={true}>Pick a degree</option>
-                <option>Diploma</option>
-                <option>Bachelor</option>
-                <option>Masters</option>
-            </select>
-            <input {...register("tution_fees")} />
-            <input {...register("application_fees")} />
-            <input {...register("service_charge")} />
-            <input {...register("application_deadline")} />
-            <input {...register("post_date")} />
-            <input {...register("posted_user_email")} />
-            <input type="submit" />
-        </form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className="fieldset">
+                    <div className="flex gap-6 my-6">
+                        <label className="label" htmlFor="name">Scholarship Name *</label>
+                        <input
+                            {...register("name", { required: true })}
+                            type="text"
+                            id="name"
+                            className="input w-full"
+                            placeholder="Scholarship Name" />
+                        {/* University Name */}
+                        <label className="label" htmlFor="university_name">University Name *</label>
+                        <input
+                            {...register("university_name", { required: true })}
+                            type="text"
+                            id="university_name"
+                            className="input w-full"
+                            placeholder="University Name" />
+                    </div>
+
+                    {/* Image/Logo */}
+                    {/* <label className="label" htmlFor="image">Image/logo *</label> */}
+                    <input {...register("image", { required: true })}
+                        type="file" className="file-input file-input-ghost w-full my-6" />
+                    {/* Country */}
+                    <label className="label" htmlFor="country">University Country *</label>
+                    <input
+                        {...register("country", { required: true })}
+                        type="text"
+                        id="country"
+                        className="input w-full mb-6"
+                        placeholder="University Country" />
+                    {/* City */}
+                    <label className="label" htmlFor="city">University City *</label>
+                    <input
+                        {...register("city", { required: true })}
+                        type="text"
+                        id="city"
+                        className="input w-full mb-6"
+                        placeholder="University City" />
+                    {/* University world rank */}
+                    <label className="label" htmlFor="world_rank">University world rank *</label>
+                    <input
+                        {...register("world_rank", { required: true })}
+                        type="text"
+                        id="world_rank"
+                        className="input w-full mb-6"
+                        placeholder="University world rank" />
+                    {/* Subject category */}
+                    {/* <legend className="fieldset-legend">Subject category</legend> */}
+                    <label className="label">Subject category *</label>
+                    <select {...register("subject_category", { required: true })}
+                        defaultValue="Pick a subject category" className="select mb-6">
+                        <option disabled={true}>Pick a subject category</option>
+                        <option>Agriculture</option>
+                        <option>Engineering</option>
+                        <option>Doctor</option>
+                    </select>
+                    {/* Scholarship category */}
+                    {/* <legend className="fieldset-legend">Scholarship category</legend> */}
+                    <label className="label">Scholarship category *</label>
+                    <select {...register("scholarship_category", { required: true })}
+                        defaultValue="Pick a scholarship category" className="select mb-6">
+                        <option disabled={true}>Pick a scholarship category</option>
+                        <option>Full-fund</option>
+                        <option>Partial</option>
+                        <option>Self-fund</option>
+                    </select>
+                    {/* Degree */}
+                    {/* <legend className="fieldset-legend">Degree</legend> */}
+                    <label className="label">Degree *</label>
+                    <select {...register("degree", { required: true })}
+                        defaultValue="Pick a degree" className="select mb-6">
+                        <option disabled={true}>Pick a degree *</option>
+                        <option>Diploma</option>
+                        <option>Bachelor</option>
+                        <option>Masters</option>
+                    </select>
+                    {/* Tution fees */}
+                    <label className="label" htmlFor="tution_fees">Tution fees</label>
+                    <input
+                        {...register("tution_fees")}
+                        type="number"
+                        id="tution_fees"
+                        className="input w-full"
+                        placeholder="Tution fees" />
+                    <p className="label mb-6">Optional</p>
+                    {/* application fees */}
+                    <label className="label" htmlFor="application_fees">Application fees *</label>
+                    <input
+                        {...register("application_fees", { required: true })}
+                        type="number"
+                        id="application_fees"
+                        className="input w-full mb-6"
+                        placeholder="Application fees" />
+                    {/* service charge */}
+                    <label className="label" htmlFor="service_charge">Service charge *</label>
+                    <input
+                        {...register("service_charge", { required: true })}
+                        type="number"
+                        id="service_charge"
+                        className="input w-full mb-6"
+                        placeholder="Service charge" />
+                    {/* application deadline */}
+                    <label className="label" htmlFor="application_deadline">Application deadline *</label>
+                    <input
+                        {...register("application_deadline", { required: true })}
+                        type="date"
+                        id="application_deadline"
+                        className="input w-full mb-6"
+                        placeholder="Application deadline" />
+                    {/* post date */}
+                    <label className="label" htmlFor="post_date">Post date *</label>
+                    <input
+                        {...register("post_date", { required: true })}
+                        type="date"
+                        id="post_date"
+                        className="input w-full mb-6"
+                        placeholder="Post date" />
+                    {/* posted user email */}
+                    <label className="label" htmlFor="post_date">Posted user email *</label>
+                    <input
+                        {...register("posted_user_email", { required: true })}
+                        type="email"
+                        id="posted_user_email"
+                        className="input w-full mb-6"
+                        placeholder="Posted user email" />
+                </fieldset>
+
+                <button type="submit" className="btn bg-teal-500 text-white btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Add Scholarship</button>
+            </form>
+        </div>
     );
 };
 
