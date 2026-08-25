fa1df014f9b36d80ad4fb7de4de100c11afa3d06
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-08-25 08:46:54 +0600

    Installed stripe, stripe for react and created cardElement

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,6 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../shared/Cover/Cover";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+// TODO: Add a publishable key
+const stripePromise = loadStripe('')
 
 const Payment = () => {
     return (
@@ -10,7 +14,11 @@ const Payment = () => {
                 {/* <title>{`Scholarship Manager | Details: ${_id}`}</title> */}
             </Helmet>
             <Cover title="Payment"></Cover>
-            <h2 className="text-4xl">Payment page</h2>
+            <div>
+                <Elements stripe={stripePromise}>
+
+                </Elements>
+            </div>
         </div>
     );
 };
