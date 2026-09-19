0d1eb671c0c18e225cd5025400e95578709a208f
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-09-19 12:18:34 +0600

    Calculated the average ratings of each of the scholarships using reduce method and displayed it on each of the cards in the home page

--- src/pages/Home/TopScholarship/TopScholarship.jsx
+++ src/pages/Home/TopScholarship/TopScholarship.jsx
@@ -6,7 +6,30 @@ const TopScholarship = () => {
     const [scholarship, loading] = useScholarship();
     console.log(scholarship);
 
+    // const { _id, university_name, image, category, country, city, application_deadline, subject_category, application_fees } = scholarship;
+    // console.log('_id:', _id);
+
+    // const axiosOpen = useAxiosOpen();
+
+    // const { data: reviews = [], isPending: isReviewsLoading } = useQuery({
+    //     // queryKey: ['reviews', _id],
+    //     queryKey: ['reviews', _id],
+    //     queryFn: async () => {
+    //         // const reviewRes = await axiosSecure(`/scholarship?scholarshipId=${id}`)
+
+    //         // TODO: use enabled to fetch request to the server when id is available
+    //         // const reviewRes = await axiosSecure(`/reviews/${_id}`)
+    //         const reviewRes = await axiosOpen(`/reviews/${_id}`)
+    //         // const reviewRes = await axiosSecure(`/reviews/${id}`)
+    //         // console.log(reviewRes.data);
+    //         return reviewRes.data;
+    //     }
+    // })
+    // console.log('reviews:', reviews);
+
+    // if (loading || isReviewsLoading) {
     if (loading) {
+        // if (isReviewsLoading) {
         return <span className="loading loading-spinner text-info"></span>
     }
 
@@ -17,6 +40,10 @@ const TopScholarship = () => {
                 subHeading={"We Provide Scholarships With Low Application Fees"}
             ></SectionTitle>
             Scholarships: {scholarship.length}
+
+            {/* <h2 className="text-3xl">Total Reviews: {reviews.length}</h2>
+            <h2 className="text-3xl">Total Rating: {reviews.rating?.length}</h2> */}
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-16">
                 {
                     scholarship.map(card => <ScholarshipCard
--- src/pages/shared/ScholarshipCard/ScholarshipCard.jsx
+++ src/pages/shared/ScholarshipCard/ScholarshipCard.jsx
@@ -1,30 +1,92 @@
 import { Link } from "react-router-dom";
+import useAxiosOpen from "../../../hooks/useAxiosOpen";
+import { useQuery } from "@tanstack/react-query";
 
 const ScholarshipCard = ({ card }) => {
 
     // TODO: const rating = 
-    const { _id, university_name, image, category, country, city, application_deadline, subject_category, application_fees } = card;
+    // const [reviews, isLoading] = useReview();
+
+    const axiosOpen = useAxiosOpen();
+
+    const { _id, name, university_name, image, category, country, city, application_deadline, subject_category, application_fees } = card;
+    console.log("_id:", _id);
+
+    const { data: reviews = [], isPending: isReviewsLoading } = useQuery({
+        // queryKey: ['reviews', _id],
+        queryKey: ['reviews', _id],
+        queryFn: async () => {
+            // const reviewRes = await axiosSecure(`/scholarship?scholarshipId=${id}`)
+
+            // TODO: use enabled to fetch request to the server when id is available
+            // const reviewRes = await axiosSecure(`/reviews/${_id}`)
+            const reviewRes = await axiosOpen(`/reviews/${_id}`)
+            // const reviewRes = await axiosSecure(`/reviews/${id}`)
+            // console.log(reviewRes.data);
+            return reviewRes.data;
+        }
+    })
+    console.log('reviews for this scholarship:', reviews);
+
+    // if (reviews.length) {
+    const totalRatings = reviews.reduce((rating, card) => rating + card.rating, 0);
+    console.log('totalRatings:', totalRatings);
+    const avg = totalRatings / reviews.length;
+    console.log('avg:', avg);
+    // }
+
+    // Method-2:
+    // const ratings = reviews.map(review => review.rating);
+    // console.log('ratings:', ratings);
+
+    // function average(numbers) {
+    //     const count = numbers.length;
+    //     console.log('numbers, count:', numbers, count);
+    //     let sum = 0;
+    //     for (const number of numbers) {
+    //         sum = sum + number;
+    //     }
+    //     const avg = sum / count;
+    //     console.log('sum:', sum);
+    //     return avg;
+    // }
+
+    // if (isLoading || isReviewsLoading) {
+    if (isReviewsLoading) {
+        return <span className="loading loading-spinner text-info"></span>
+    }
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-            <figure>
-                <img
-                    src={image}
-                    className="h-42"
-                    alt="image" />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{university_name}</h2>
-                <p>{category}</p>
-                <p>{city}, {country}</p>
-                <p>{application_deadline}</p>
-                <p>{subject_category}</p>
-                <p>{application_fees}</p>
-                <p>Rating: (just rating point it should be the average rating point of all rating points),{}</p>
-                <div className="card-actions justify-end">
-                    <Link to={`/scholarship/${_id}`}>
-                        <button className="btn bg-cyan-500 text-white">Scholarship Details</button>
-                    </Link>
+        <div>
+            <h2 className="text-cyan-400">Name: {name}</h2>
+            <h2 className="text-cyan-400">Total Reviews: {reviews.length}</h2>
+            {/* <h2 className="text-cyan-400">Total Ratings: {ratings?.length}</h2> */}
+            <h2 className="text-cyan-400">Total Ratings: {totalRatings}</h2>
+            {/* <h2 className="text-cyan-400">Average Ratings: {avg}</h2> */}
+
+            <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                        src={image}
+                        className="h-42"
+                        alt="image" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">{university_name}</h2>
+                    <p>{category}</p>
+                    <p>{city}, {country}</p>
+                    <p>{application_deadline}</p>
+                    <p>{subject_category}</p>
+                    <p>{application_fees}</p>
+                    {/* <p>Rating: (just rating point it should be the average rating point of all rating points)</p> */}
+                    {/* Method-2 */}
+                    {/* <p>Rating: {ratings.length && average(ratings)}</p> */}
+                    <p>Rating: {reviews.length && avg}</p>
+                    <div className="card-actions justify-end">
+                        <Link to={`/scholarship/${_id}`}>
+                            <button className="btn bg-cyan-500 text-white">Scholarship Details</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
